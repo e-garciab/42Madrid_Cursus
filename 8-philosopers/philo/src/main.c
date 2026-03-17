9b3eefdca516f6794d0a4fd9645a866770644a75
e-garciab <elisagarciabarbazan@gmail.com>
2026-03-17 20:29:17 +0100

    philo

---
+++ 8-philosopers/philo/src/main.c
@@ -0,0 +1,82 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: egarcia2 <egarcia2@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/08 10:02:51 by egarcia2          #+#    #+#             */
+/*   Updated: 2026/03/15 21:14:54 by egarcia2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+// ./philo number_of_philosophers time_to_die time_to_eat time_to_sleep [number_of_times_each_philosopher_must_eat]
+
+int main (int argc, char *argv[])
+{
+    t_data data;
+    pthread_t monitor;
+        
+    if(!check_args(argc, argv, &data))
+        return(1);
+    if(!init_data(&data))
+    {
+        cleanup (&data);
+        return(1);
+    }
+    printf("filosofos: %d | Die: %ld | Eat: %ld | Sleep: %ld | Must eat: %d\n", data.nbr_philos, data.time_to_die, data.time_to_eat, data.time_to_sleep, data.nbr_must_eat);
+    if(!launch_philos(&data))
+    {
+        cleanup (&data);
+        return(1);  
+    }
+    if(pthread_create(&monitor, NULL, monitor_routine, &data) != 0)
+    {
+       cleanup (&data);
+       return(1);   
+    }
+    if(pthread_join(monitor, NULL) != 0)
+    {
+       cleanup (&data);
+       return(1);  
+    }
+    if(!join_philos(&data))
+    {
+        cleanup (&data);
+        return(1);  
+    }
+    cleanup (&data);
+    return(0);   
+}
+    
+
+
+
+
+
+
+// void *routine()
+// {
+//     printf("Test from threads\n");
+//     sleep (3);
+//     printf("Ending thread\n");
+// }
+
+
+// int main(int argc, char *argv[])
+//  {
+//     pthread_t   t1;
+//     pthread_t   t2;
+
+//     if(pthread_create(&t1, NULL, &routine, NULL) != 0)
+//         return 1;
+//     if(pthread_create(&t2, NULL, &routine, NULL) != 0)
+//          return 2;
+//     if(pthread_join(t1, NULL) != 0)
+//          return 3;
+//     if(pthread_join(t2, NULL) != 0)
+//         return 4;
+//     return(0);
+//  }
---
+++ 8-philosopers/philo/src/parser_utils.c
@@ -0,0 +1,70 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parser_utils.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: egarcia2 <egarcia2@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/10 22:21:53 by egarcia2          #+#    #+#             */
+/*   Updated: 2026/03/10 22:23:13 by egarcia2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+static int	ft_isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	return (0);
+}
+
+static int	ft_check_sign(char *str, int *i)
+{
+	int	sign;
+
+	sign = 1;
+	if (str[*i] == '+' || str[*i] == '-')
+	{
+		if (str[*i] == '-')
+			sign = -1;
+		(*i)++;
+	}
+	return (sign);
+}
+
+static int	ft_check_limits(long result, int sign)
+{
+	if ((sign == 1 && result > INT_MAX) || (sign == -1 && result > (long)INT_MAX
+			+ 1))
+	{
+		return (0);
+	}
+	return (1);
+}
+
+int	ft_atoi_safe(char *str, long *out)
+{
+	int		i;
+	long	result;
+	int		sign;
+
+	i = 0;
+	result = 0;
+	if (!str || str[0] == '\0')
+		return (0);
+	while ((str[i]) && (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13)))
+		i++;
+	sign = ft_check_sign(str, &i);
+	if (!ft_isdigit(str[i]))
+		return (0);
+	while (ft_isdigit(str[i]))
+	{
+		result = result * 10 + (str[i] - '0');
+		if (!ft_check_limits(result, sign))
+			return (0);
+		i++;
+	}
+	*out = (result * sign);
+	return (1);
+}
